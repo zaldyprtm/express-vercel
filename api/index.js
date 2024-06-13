09dd86a8eaefd951e40b89f8b7a24b51e45e8afb
zaldyprtm <zaldyprtm@gmail.com>
2024-06-13 14:35:17 +0700

    'fix'

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
-
-const express = require("express");
-const app = express();
+const express = require('express');
 const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
 
-
+// Configure CORS to allow requests from your frontend URL without trailing slash
 const corsOptions = {
-  origin: 'https://frontend-seven-mauve-63.vercel.app/', // Ganti dengan URL frontend yang benar
+  origin: 'https://frontend-seven-mauve-63.vercel.app/', // URL frontend tanpa trailing slash
   optionsSuccessStatus: 200
 };
 
@@ -14,7 +14,7 @@ app.use(cors(corsOptions));
 app.get('/api/data', (req, res) => {
   res.json({ message: 'Hello from Express!' });
 });
-app.listen(8080, () => {
-  console.log("Server started at http://localhost:8080");
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
-module.exports = app;
