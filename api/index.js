d3bbfeba876c98a9c1f008d235577a0da58a69d9
zaldyprtm <zaldyprtm@gmail.com>
2024-06-13 14:31:47 +0700

    'fix'

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 
 const express = require("express");
 const app = express();
+const cors = require('cors');
+
+
+const corsOptions = {
+  origin: 'https://frontend-seven-mauve-63.vercel.app/', // Ganti dengan URL frontend yang benar
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
 app.get('/api/data', (req, res) => {
   res.json({ message: 'Hello from Express!' });
 });
